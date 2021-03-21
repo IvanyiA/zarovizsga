97807bcd047fadc7a768442e6676e934f908d4bc
IvanyiA <trachemys13@gmail.com>
2021-03-21 15:54:48 +0100

    WorkHours külön osztály használata nélkül

--- src/main/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHours.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHours.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDate;
+import java.util.List;
 
 public class WorkHours {
 
@@ -19,6 +20,7 @@ public class WorkHours {
                 String name = parts[0];
                 int workHours = Integer.parseInt(parts[1]);
                 LocalDate date = LocalDate.parse(parts[2]);
+
                 Worker actual = new Worker(name, workHours, date);
                 if (laziest.getWorkHours() > actual.getWorkHours()) {
                     laziest = actual;
@@ -29,4 +31,32 @@ public class WorkHours {
         }
         return laziest.toString();
     }
+
+    public String minWork2(String path) {
+        try {
+            List<String> lines = Files.readAllLines(Path.of(path));
+
+            return findMin(lines);
+
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Cannot read file", ioe);
+        }
+    }
+
+    private String findMin(List<String> lines) {
+        int minHours = 25;
+        String result = null;
+        for (String line : lines) {
+            String[] parts = line.split(",");
+            String name = parts[0];
+            int workHours = Integer.parseInt(parts[1]);
+            String date = parts[2];
+            if (minHours > workHours) {
+                minHours = workHours;
+                result = name + ": " + date;
+            }
+        }
+        return result;
+    }
+
 }
